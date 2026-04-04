bb253658f132713012ca19c4602978e92b124c33
rayhong <rayhong118@gmail.com>
2026-04-03 17:41:51 -0700

    CH1 L7

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -2,7 +2,7 @@ import { type BunRequest } from "bun";
 import { getBearerToken, validateJWT } from "../auth";
 import type { ApiConfig } from "../config";
 import { getVideo, updateVideo } from "../db/videos";
-import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
+import { BadRequestError, UserForbiddenError } from "./errors";
 import { respondWithJSON } from "./json";
 
 type Thumbnail = {
@@ -12,32 +12,6 @@ type Thumbnail = {
 
 // limit 20 MB
 const MAX_UPLOAD_SIZE = 20 << 20;
-
-const videoThumbnails: Map<string, Thumbnail> = new Map();
-
-export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
-  const { videoId } = req.params as { videoId?: string };
-  if (!videoId) {
-    throw new BadRequestError("Invalid video ID");
-  }
-
-  const video = getVideo(cfg.db, videoId);
-  if (!video) {
-    throw new NotFoundError("Couldn't find video");
-  }
-
-  const thumbnail = videoThumbnails.get(videoId);
-  if (!thumbnail) {
-    throw new NotFoundError("Thumbnail not found");
-  }
-
-  return new Response(thumbnail.data, {
-    headers: {
-      "Content-Type": thumbnail.mediaType,
-      "Cache-Control": "no-store",
-    },
-  });
-}
 
 export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
@@ -64,17 +38,17 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const mediaType = thumbnail.type;
 
   const imageData = await thumbnail.arrayBuffer();
+  const buffer = Buffer.from(imageData).toString("base64");
+  const dataURL = `data:${mediaType};base64,${buffer}`;
 
   const videoMetadata = getVideo(cfg.db, videoId);
   if (videoMetadata?.userID !== userID) {
     throw new UserForbiddenError("");
   }
 
-  videoThumbnails.set(videoId, { data: imageData, mediaType });
+  // const thumbnailURL = `http://localhost:${cfg.port}/api/thumbnails/${videoId}`;
 
-  const thumbnailURL = `http://localhost:${cfg.port}/api/thumbnails/${videoId}`;
-
-  videoMetadata.thumbnailURL = thumbnailURL;
+  videoMetadata.thumbnailURL = dataURL;
 
   await updateVideo(cfg.db, videoMetadata);
 
