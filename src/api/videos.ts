f72def7ee018745111c832833eaabf3e887a5840
rayhong <rayhong118@gmail.com>
2026-04-05 16:32:07 -0700

    ch4 L3

--- src/api/videos.ts
+++ src/api/videos.ts
@@ -47,7 +47,9 @@ export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
   const tempFilePath = path.join("/tmp", `${videoId}.mp4`);
   await Bun.write(tempFilePath, file);
 
-  let key = `${videoId}.mp4`;
+  const videoAspectRatio = await getVideoAspectRatio(tempFilePath);
+
+  let key = `${videoAspectRatio}${videoId}.mp4`;
   await uploadVideoToS3(cfg, key, tempFilePath, "video/mp4");
 
   const videoURL = `https://${cfg.s3Bucket}.s3.${cfg.s3Region}.amazonaws.com/${key}`;
@@ -57,4 +59,46 @@ export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
   await Promise.all([rm(tempFilePath, { force: true })]);
 
   return respondWithJSON(200, video);
+}
+
+export async function getVideoAspectRatio(filePath: string) {
+  const cmd = [
+    "ffprobe",
+    "-v",
+    "error",
+    "-select_streams",
+    "v:0",
+    "-show_entries",
+    "stream=width,height",
+    "-of",
+    "json",
+    filePath,
+  ];
+
+  const { stdout, stderr, exited } = Bun.spawn(cmd);
+  const stdoutText = await new Response(stdout).text();
+  const exitedResult = await exited;
+  if (exitedResult !== 0) {
+    const stderrText = await new Response(stderr).text();
+    throw new Error(`ffprobe failed: ${stderrText}`);
+  }
+
+  const stdoutJson = JSON.parse(stdoutText);
+
+  // Extract width and height from the streams array
+  const width = stdoutJson.streams[0].width;
+  const height = stdoutJson.streams[0].height;
+
+  return getRatioType(width, height);
+}
+
+function getRatioType(width: number, height: number): string {
+  if (Math.floor(width / 16) == Math.floor(height / 9)) {
+    return "landscape/";
+  }
+  if (Math.floor(width / 9) == Math.floor(height / 16)) {
+    return "portrait/";
+  }
+
+  return "other";
 }
